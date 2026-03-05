e9ed458524a914095c9c54592f7cfa96aed11491
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-03-05 23:46:48 +0530

    Adding database security

--- src/main/java/com/Spring/SecurityDemo/Config/SecurityConfig.java
+++ src/main/java/com/Spring/SecurityDemo/Config/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.Spring.SecurityDemo.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -28,7 +33,27 @@ public class SecurityConfig {
     // UserDetailsService is used for multiple user....
     @Bean
     public UserDetailsService userDetailsService(){
-        return new InMemoryUserDetailsManager();
+
+        UserDetails user1 =  User.withDefaultPasswordEncoder()
+                .username("nimesh")
+                .password("nimesh")
+                .roles("USER")
+                .build();
+        UserDetails user2 =  User.withDefaultPasswordEncoder()
+                .username("vishal")
+                .password("vishal")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1,user2);
     }
+
+
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder);
+        return provider;
+
+    }
+
 
 }
